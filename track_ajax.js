bfcb8a543374800ce3d62483a8770383b9382c1b
Denis Seleznev <hcodes@yandex.ru>
2013-09-16 22:20:14 +0400

    черновик

---
+++ track_ajax.js
@@ -0,0 +1,70 @@
+;
+(function (window, document) {
+	var originalXMLHttpRequest = window.XMLHttpRequest;
+	var TrackAjax = {
+		init: function () {
+			if (!this._has()) {
+				return;
+			}
+			
+			this.initAjax();
+			this.initJSONP();
+			
+			this.initEvents();
+		},
+		has: function () {
+			return !(originalXMLHttpRequest && new originalXMLHttpRequest.addEventListener);
+		},
+		_counters: [],
+		updateCounters: function () {
+			if (window.Ya && window.Ya.Metrika) {
+				this._counters = window.Ya.Metrika.counters();
+			}
+			
+			return this._counters;
+		},
+		initAjax: function () {
+			window.XMLHttpRequest = function (params) {
+				var obj = new originalXMLHttpRequest(params);
+				var that = this;
+				obj.addEventListener('readystatechange', function (e) {
+					if (e.readyState >= 3 && e.status >= 400) {
+						that.send('Ajax errors', that.isCrossDomain() ? 'Crossdomain ajax' : 'Ajax', e.status, e.url, 1);
+					}
+				}, false);
+				
+				return obj;
+			};
+		},
+		initJSONP: function () {
+			document.addEventListener('error', function (e) {
+				var target = e.target;
+				var tagName = ('' + target.tagName).toLowerCase();
+				
+				if (target && tagName == 'SCRIPT') { // TODO
+					var src = '' + el.src;
+					if (src.search(/(\?|&)callback=/) !== -1) {
+						that.send('Ajax errors', 'JSONP', src, 1);
+					}
+				}
+				
+			}, true);
+		},
+		send: function () {
+			this.updateCounters();
+			
+			for (var i = 0; i < this._counters.length; i++) {
+				window['yaCounter' + this._counters[i]].params.apply(this, arguments);
+			}
+		},
+		getDomain: function (url) {
+			return domain; // TODO
+		},
+		isCrossDomain: function (url) {
+			return this.getDomain(window.location.href) == this.getDomain(url);
+		},
+		sameDomain: function (url1, url2) {
+			return this.getDomain(url1) == this.getDomain(url2);
+		}
+	};
+})(this, document);
