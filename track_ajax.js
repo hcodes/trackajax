5fd7fd5bd474efddb5d61591407740487cc3d3bd
Denis Seleznev <hcodes@yandex.ru>
2014-09-30 14:09:00 +0400

    Рефакторинг

--- track_ajax.js
+++ track_ajax.js
@@ -1,81 +1,105 @@
-;
-(function (window, document) {
-	var originalXMLHttpRequest = window.XMLHttpRequest;
-	var TrackAjax = {
-		init: function () {
-			if (!this._has()) {
-				return;
-			}
-			
-			this.initAjax();
-			this.initJSONP();
-			
-			this.initEvents();
-		},
-		has: function () {
-			return !(originalXMLHttpRequest && new originalXMLHttpRequest.addEventListener);
-		},
-		_counters: [],
-		updateCounters: function () {
-			if (window.Ya && window.Ya.Metrika) {
-				this._counters = window.Ya.Metrika.counters();
-			}
-			
-			return this._counters;
-		},
-		initAjax: function () {
-			window.XMLHttpRequest = function (params) {
-				var obj = new originalXMLHttpRequest(params);
-				var that = this;
-				obj.addEventListener('readystatechange', function (e) {
-					if (e.readyState >= 3 && e.status >= 400) {
-						that.send('Ajax errors', that.isCrossDomain() ? 'Crossdomain ajax' : 'Ajax', e.status, e.url, 1);
-					}
-				}, false);
-				
-				return obj;
-			};
-		},
-		initJSONP: function () {
-			document.addEventListener('error', function (e) {
-				var target = e.target;
-				var tagName = ('' + target.tagName).toLowerCase();
-				
-				if (target && tagName == 'SCRIPT') { // TODO
-					var src = '' + el.src;
-					if (src.search(/(\?|&)callback=/) !== -1) {
-						that.send('Ajax errors', 'JSONP', src, 1);
-					}
-				}
-				
-			}, true);
-		},
-		_buffer: [],
-		send: function () {
-			this.updateCounters();
-			
-			if (arguments[0]) {
-				this._buffer.push(arguments);
-			}
-			
-			if (this._counter.length) {
-				for (var n = 0; n < this._buffer[n]; n++) {
-					for (var i = 0; i < this._counters.length; i++) {
-						window['yaCounter' + this._counters[i]].params.apply(this, buf[n]);
-					}
-				}
-				
-				this._buffer = [];
-			}
-		},
-		getDomain: function (url) {
-			return domain; // TODO
-		},
-		isCrossDomain: function (url) {
-			return this.getDomain(window.location.href) == this.getDomain(url);
-		},
-		sameDomain: function (url1, url2) {
-			return this.getDomain(url1) == this.getDomain(url2);
-		}
-	};
+var TrackAjax = (function (window, document) {
+    var originalXMLHttpRequest = window.XMLHttpRequest,
+        buffer = [],
+        hasXHR = !!(originalXMLHttpRequest && new originalXMLHttpRequest.addEventListener),
+        link = document.createElement('a'),
+        inited = false;
+
+    function isCrossDomain(url) {
+        link.href = url;
+        return window.location.host !== link.host;
+    }
+
+    return {
+        on: function (counterId) {
+            var that = this;
+
+            if(hasXHR || inited) {
+                return;
+            }
+
+            that._counterId = counterId;
+
+            inited = true;
+            buffer = [];
+            that._xhrs = [];
+
+            // Ajax and crossdomain ajax
+            that._onreadychange = function(e) {
+                if(e.readyState >= 3 && e.status >= 400) {
+                    that._sendError(isCrossDomain(e.url) ? 'Crossdomain' : 'Samedomain', e.status, e.url, 1);
+                }
+            };
+
+            window.XMLHttpRequest = function(params) {
+                var xhr = new originalXMLHttpRequest(params);
+                that._xhrs.push(xhr);
+                xhr.addEventListener('readystatechange', that._onreadychange, false);
+
+                return xhr;
+            };
+
+            // JSONP
+            that._onerror = function(e) {
+                var target = e.target,
+                     tagName = ('' + target.tagName).toLowerCase();
+
+                if(target && tagName === 'script') {
+                    var src = '' + el.src;
+                    if(src.search(/(\?|&)callback=/) !== -1) {
+                        that._sendError('JSONP', src, 1);
+                    }
+                }
+            };
+
+            document.addEventListener('error', onerror, true);
+        };
+
+        off: function() {
+            if(inited) {
+                for(var i = 0; i < this._xhrs.length; i++) {
+                    this._xhrs[i].removeEventListener('readystatechange', this._onreadychange, false);
+                }
+
+                this._xhrs = [];
+                window.XMLHttpRequest = originalXMLHttpRequest;
+
+                document.removeEventListener('error', this._onerror, true);
+
+                buffer = [];
+                delete this._counterId;
+                inited = false;
+            }
+        },
+        _getCounterId: function() {
+            var id = this._counterId,
+                wya = window.Ya;
+
+            if(id) {
+                return id;
+            } else if(wya && wya.Metrika && wya.Metrika.counters) {
+                var buf = wya.Metrika.counters();
+                return buf && buf[0].id;
+            }
+
+            return false;
+        },
+        _sendError: function() {
+            if(arguments[0]) {
+                buffer.push(arguments);
+            }
+
+            var counterId = this._getCounterId(),
+                ya;
+
+            if(counterId) {
+                ya = window['yaCounter' + counterId];
+                for (var i = 0; i < buffer.length; i++) {
+                    ya.params.apply(ya, ['Ajax errors'].concat(buffer[i]));
+                }
+
+                buffer = [];
+            }
+        }
+    }
 })(this, document);
